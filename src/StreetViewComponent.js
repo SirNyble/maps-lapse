365b01781854c0c56416f5c348280f0f835b6f7c
SirNyble <thomasrhys14@gmail.com>
2018-02-07 22:03:09 -0400

    Now textured the streetview equirectangular image onto a sphere so users can freely look around
    Basically should look similar to google streetview, unless you zoom out

--- src/StreetViewComponent.js
+++ src/StreetViewComponent.js
@@ -10,10 +10,9 @@ const GOOGLE_API_KEY = 'AIzaSyA8AxDTe-ds-ZNV3omvTcm9fXUtEyv0Z0k';
  */
 class StreetViewComponent extends React.Component {
   constructor(props) {
-
     super(props);
 
-    this.zoom = 1.0;
+    this.zoom = 3;
     this.panoNumCols = Math.pow(2, this.zoom);
     this.panoNumRows = Math.pow(2, this.zoom - 1);
 
@@ -23,96 +22,11 @@ class StreetViewComponent extends React.Component {
     this.createPanoramaTiles = this.createPanoramaTiles.bind(this);
     this.createPanorama = this.createPanorama.bind(this);
 
-      // window.renderMap = this.renderMap.bind(this);
-      // var api = 'AIzaSyCllUWAbg1ZHiimQm5nfp4Z5JvJ7lRhknU';
-      // var s = document.createElement('script');
-      // s.src = '//maps.googleapis.com/maps/api/js?key=' + api + '&callback=renderMap';
-      // s.type = 'text/javascript';
-      // s.id = 'gmaps-api';
-      // document.getElementsByTagName("head")[0].appendChild(s);
-
     this.streetViewSphereTexture = null;
     this.camera = null;
     this.scene = null;
     this.renderer = null;
     this.skybox = null;
-  }
-
-  renderMap() {
-    // var berkeley = {lat:44.672682, lng:  -63.610158};
-    // var sv = new google.maps.StreetViewService();
-    // console.log(sv);
-    // //const panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'));
-
-    // Set up the map.
-    // map = new google.maps.Map(document.getElementById('map'), {
-    //   center: berkeley,
-    //   zoom: 16,
-    //   streetViewControl: false
-    // });
-    //
-    // // Set the initial Street View camera to the center of the map
-    // sv.getPanorama({location: berkeley, radius: 150}, (panData, svStatus) => {
-    //   console.log(panData.tiles);
-    //   console.log("-asdfsadf-s-----------");
-    //   console.log(panData.location.pano);
-    //   load(panData.location.pano, { zoom: 2 })
-    //   .on('start', function (data) {
-    //     console.log('canvas size: ', data.width, data.height)
-    //   })
-    //   .on('progress', function (ev) {
-    //     console.log('progress: ', ev.count / ev.total)
-    //   })
-    //   .on('complete', function (image) {
-    //     document.body.appendChild(image)
-    //     console.log('canvas image: ', image)
-    //     image.setAttribute('crossOrigin', 'Anonymous');
-    //     const blah = image.toDataURL();
-    //     console.log(blah);
-    //
-    //     let canvas = document.createElement('canvas');
-    //     let ctx = canvas.getContext('2d');
-    //     ctx.drawImage(image, image.width, image.height);
-    //
-    //     this.streetViewSphereTexture = new THREE.Texture(canvas);
-    //     this.streetViewSphereTexture.needsUpdate = true;
-    //
-    //     let sphereGeom = new THREE.SphereGeometry(10, 32, 32);
-    //     let sphereMat = new THREE.MeshBasicMaterial({map: this.streetViewSphereTexture, side: THREE.DoubleSide});
-    //     this.sphere = new THREE.Mesh(sphereGeom, sphereMat);
-    //     //this.streetViewSphereTexture.needsUpdate = true;
-    //     this.scene.add(this.sphere);
-    //             console.log(this.sphere);
-
-
-        // const dataURL = image.toDataURL();
-        // let img = new Image();
-        // img.on('load', function() {
-        //   this.streetViewSphereTexture = new THREE.Texture(image);
-        //   this.streetViewSphereTexture.needsUpdate = true;
-        //
-        //   let sphereGeom = new THREE.SphereGeometry(10, 32, 32);
-        //   let sphereMat = new THREE.MeshBasicMaterial({map: this.streetViewSphereTexture, side: THREE.DoubleSide});
-        //   this.sphere = new THREE.Mesh(sphereGeom, sphereMat);
-        //   //this.streetViewSphereTexture.needsUpdate = true;
-        //   this.scene.add(this.sphere);
-        //           console.log(this.sphere);
-        // }.bind(this));
-        // img.setAttribute('crossOrigin', 'anonymous');
-        // img.src = dataURL;
-
-        //this.sphere.material.needsUpdate = true; this.sphere.material.needsUpdate = true
-    //
-    //   }.bind(this))
-    // });
-
-    // Look for a nearby Street View panorama when the map is clicked.
-    // getPanoramaByLocation will return the nearest pano when the
-    // given radius is 50 meters or less.
-    // map.addListener('click', function(event) {
-    //   sv.getPanorama({location: event.latLng, radius: 50}, processSVData);
-    // });
-
   }
 
   animate() {
@@ -123,9 +37,10 @@ class StreetViewComponent extends React.Component {
 
   componentDidMount() {
     const container = document.getElementById( 'webgl-mount' );
+    this.panoramaCanvas = document.getElementById( 'panoramaCanvas' );
 
     this.camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.01, 1000 );
-    this.camera.position.set(0, 0, 15.1);
+    this.camera.position.set(0, 0, 0.1);
     this.camera.lookAt(new THREE.Vector3());
 
     this.controls = new OrbitControls(this.camera);
@@ -134,6 +49,13 @@ class StreetViewComponent extends React.Component {
 
     this.renderer = new THREE.WebGLRenderer( { antialias: true } );
     this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+    let sphereGeom = new THREE.SphereGeometry(10, 32, 32);
+    this.streetViewSphereTexture = new THREE.CanvasTexture(this.panoramaCanvas);
+    let sphereMat = new THREE.MeshBasicMaterial({map: this.streetViewSphereTexture, side: THREE.DoubleSide});
+    this.sphere = new THREE.Mesh(sphereGeom, sphereMat);
+    //this.streetViewSphereTexture.needsUpdate = true;
+    this.scene.add(this.sphere);
 
     this.getStreetViewTexture();
 
@@ -172,7 +94,7 @@ class StreetViewComponent extends React.Component {
     const long = -63.610197;
     axios.get('http://maps.google.com/cbk?output=json&ll=' + lat + ',' + long + '&dm=0')
     .then(res => {
-      console.log(res.data.Location.panoId);
+      console.log(res.data);
       this.createPanoramaTiles(res.data.Location.panoId);
     }).catch(err => {
         console.log(err);
@@ -189,9 +111,13 @@ class StreetViewComponent extends React.Component {
           {
             responseType: 'arraybuffer'
           })
+          .catch(err => {
+            console.log(err);
+          })
         .then(response => new Buffer(response.data, 'binary').toString('base64'))
+
         .then(tile => {
-          console.log(tile);
+          //console.log(tile);
           let img = new Image();
           img.onload = function() {
             console.log('tile loaded!');
@@ -205,26 +131,29 @@ class StreetViewComponent extends React.Component {
               console.log('done loading');
               this.createPanorama(imageArr);
             }
-            document.body.appendChild(img);
           }.bind(this)
           img.src = 'data:image/png;base64,' + tile;
-
+        })
+        .catch(err => {
+          imagesLoaded++;
+          console.log(err);
         })
       }
     }
   }
 
   createPanorama(imgArr) {
-    let canvas = document.getElementById('panorama');
-    let ctx = canvas.getContext('2d');
-    canvas.width = 416 * this.panoNumCols;
-    canvas.height = 416 * this.panoNumRows;
-    console.log(imgArr);
-    console.log("FSDF");
+
+    let ctx = this.panoramaCanvas.getContext('2d');
+    this.panoramaCanvas.width = 416 * this.panoNumCols;
+    this.panoramaCanvas.height = 416 * this.panoNumRows;
+
     for(let imgTile = 0; imgTile < imgArr.length; imgTile++){
       console.log(imgArr[imgTile].img);
       ctx.drawImage(imgArr[imgTile].img, imgArr[imgTile].x * 512, imgArr[imgTile].y * 512);
     }
+    this.streetViewSphereTexture.needsUpdate = true;
+
   }
 
 
@@ -237,7 +166,7 @@ class StreetViewComponent extends React.Component {
     return (
       <div>
         <div id="webgl-mount" onClick={this.handleClick}></div>
-        <canvas id="panorama" />
+        <canvas id="panoramaCanvas" />
       </div>
     );
   }
